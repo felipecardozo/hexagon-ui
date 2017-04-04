b57be50fa0f7ed86b038339687d9f147db0aa997
Felipe Cardozo <fcardozo@corp.affinitas.de>
2017-04-04 17:37:21 +0200

    Horrible else if to change color to Complete

--- app.js
+++ app.js
@@ -1,31 +1,84 @@
+var COMPLETE = "rgb(175, 17, 17)";
+var INICIAL = "#eff595";
+var GESTIONADO = "#e8ea51";
+var DEFINIDO = "rgb(188, 225, 239)";
+var CONTROLADO = "#99d1de";
+var OPTIMIZADO = "#89cc78";
+
 function changeModel() {
 	var radios = $("input[name='options']:checked");
 	radios.forea
 	jQuery.each(radios, function(i, val) {
-		console.log(val.value);
+
 		if (val.value == "1-1") {
-			$(".pyramid-container99-0 .pyramid-level-22").css("border-bottom-color", "rgb(175, 17, 17)");
+			changeColorToComplete(".pyramid-container99-0 .pyramid-level-22");
 		} else if (val.value == "1-2") {
-			$(".pyramid-container99-0 .pyramid-level-33").css("border-bottom-color", "rgb(175, 17, 17)");
+			changeColorToComplete(".pyramid-container99-0 .pyramid-level-33");
 		} else if (val.value == "1-3") {
-			$(".pyramid-container99-0 .pyramid-level-4").css("border-bottom-color", "rgb(175, 17, 17)");
+			changeColorToComplete(".pyramid-container99-0 .pyramid-level-4");
 		} else if (val.value == "1-4") {
-			$(".pyramid-container99-0 .pyramid-level-5").css("border-bottom-color", "rgb(175, 17, 17)");
+			changeColorToComplete(".pyramid-container99-0 .pyramid-level-5");
 		} else if (val.value == "1-5") {
-			$(".pyramid-container99-0 .pyramid-level-6").css("border-bottom-color", "rgb(175, 17, 17)");
+			changeColorToComplete(".pyramid-container99-1 .pyramid-level-6");
 		} else if (val.value == "2-1") {
-
+			changeColorToComplete(".pyramid-container99-1 .pyramid-level-22");
 		} else if (val.value == "2-2") {
-
+			changeColorToComplete(".pyramid-container99-1 .pyramid-level-33");
 		} else if (val.value == "2-3") {
-
+			changeColorToComplete(".pyramid-container99-1 .pyramid-level-4");
 		} else if (val.value == "2-4") {
-
+			changeColorToComplete(".pyramid-container99-1 .pyramid-level-5");
 		} else if (val.value == "2-5") {
-
+			changeColorToComplete(".pyramid-container99-1 .pyramid-level-6");
 		} else if (val.value == "3-1") {
-
+			changeColorToComplete(".pyramid-container99-2 .pyramid-level-22");
+		} else if (val.value == "3-2") {
+			changeColorToComplete(".pyramid-container99-2 .pyramid-level-33");
+		} else if (val.value == "3-3") {
+			changeColorToComplete(".pyramid-container99-2 .pyramid-level-4");
+		} else if (val.value == "3-4") {
+			changeColorToComplete(".pyramid-container99-2 .pyramid-level-5");
+		} else if (val.value == "3-5") {
+			changeColorToComplete(".pyramid-container99-2 .pyramid-level-6");
+		} else if (val.value == "4-1") {
+			changeColorToComplete(".pyramid-container99-3 .pyramid-level-22");
+		} else if (val.value == "4-2") {
+			changeColorToComplete(".pyramid-container99-3 .pyramid-level-33");
+		} else if (val.value == "4-3") {
+			changeColorToComplete(".pyramid-container99-3 .pyramid-level-4");
+		} else if (val.value == "4-4") {
+			changeColorToComplete(".pyramid-container99-3 .pyramid-level-5");
+		} else if (val.value == "4-5") {
+			changeColorToComplete(".pyramid-container99-3 .pyramid-level-6");
+		} else if (val.value == "5-1") {
+			changeColorToComplete(".pyramid-container99-4 .pyramid-level-22");
+		} else if (val.value == "5-2") {
+			changeColorToComplete(".pyramid-container99-4 .pyramid-level-33");
+		} else if (val.value == "5-3") {
+			changeColorToComplete(".pyramid-container99-4 .pyramid-level-4");
+		} else if (val.value == "5-4") {
+			changeColorToComplete(".pyramid-container99-4 .pyramid-level-5");
+		} else if (val.value == "5-5") {
+			changeColorToComplete(".pyramid-container99-4 .pyramid-level-6");
+		} else if (val.value == "6-1") {
+			changeColorToComplete(".pyramid-container99-5 .pyramid-level-22");
+		} else if (val.value == "6-2") {
+			changeColorToComplete(".pyramid-container99-5 .pyramid-level-33");
+		} else if (val.value == "6-3") {
+			changeColorToComplete(".pyramid-container99-5 .pyramid-level-4");
+		} else if (val.value == "6-4") {
+			changeColorToComplete(".pyramid-container99-5 .pyramid-level-5");
+		} else if (val.value == "6-5") {
+			changeColorToComplete(".pyramid-container99-5 .pyramid-level-6");
 		}
 	});
 
+}
+
+function changeColorToComplete(selector) {
+	$(selector).css("border-bottom-color", COMPLETE);
+}
+
+function changeToDefaultColor(selector, color) {
+	$(selector).css("border-bottom-color", color);
 }
